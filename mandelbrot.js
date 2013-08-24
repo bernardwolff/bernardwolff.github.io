e3de0a2ddd5a01bd393ca3c3b745e5bfee3468c1
Bernard Wolff <bernard.wolff@tntp.org>
2013-08-23 21:45:35 -0400

    more cleanup

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,30 +1,16 @@
 function draw_mandelbrot(context, params) {
-  var mandelbrot_scale = function () {
-    self = this;
-    self.x = {min: -2.5, max: 1};
-    self.y = {min: -1, max: 1};
-
-    self.scale_coord = function(coord, min, max, w) {
-      // note: this function is not returning numbers equal to the max, need to fix
-      return ((max - min) * coord / w) + min;
-    };
-
-    self.scale_point = function (point) {
-      return {
-        x: self.scale_coord(point.x, self.x.min, self.x.max, context.canvas.width),
-          y: self.scale_coord(point.y, self.y.min, self.y.max, context.canvas.height),
-      };
+  function scale_point(point) {
+    var xmin = -2.5, xmax = 1, ymin = -1, ymax = 1;
+    return {
+      x: ((xmax - xmin) * point.x / context.canvas.width) + xmin,
+        y: ((ymax - ymin) * point.y / context.canvas.height) + ymin
     };
   }
 
-  var ms = new mandelbrot_scale();
-
   for (var i = 0; i < context.canvas.width; i++) {
     for (var j = 0; j < context.canvas.height; j++) {
-      var scaled_point = ms.scale_point({x: i, y: j});
-      var x = 0, y = 0;
-
-      var iteration = 0;
+      var scaled_point = scale_point({x: i, y: j});
+      var x = 0, y = 0, iteration = 0;
 
       while (x*x + y*y < 2*2 && iteration < params.max_iteration) {
         var xtemp = x*x - y*y + scaled_point.x;
