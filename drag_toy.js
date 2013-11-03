fde53dd9872a91c7b0b4f2e6ea52eb14dc691b00
Bernard Wolff <bernard.wolff@tntp.org>
2013-11-03 14:12:12 -0800

    three part chain

--- drag_toy.js
+++ drag_toy.js
@@ -3,20 +3,17 @@ function draw_drag_toy() {
   var canvas = $canvas[0];
   var context = canvas.getContext("2d");
 
-  /*var draggable_segment1 =
-    new DraggableSegment(new DraggableCircle(5, 200, 100, 'blue', '#000000'),
-          new DraggableCircle(5, 200, 150, 'brown', '#000000'));*/
-
-  var draggable_segment2 =
-    new DraggableSegment(new DraggableCircle(5, 100, 100, 'blue', '#000000'),
-          new DraggableCircle(5, 150, 150, 'brown', '#000000'));
+  var chain = new DraggableChain([
+    new Link(5, 100, 100, 'blue', '#000000'),
+    new Link(5, 150, 150, 'red', '#000000'),
+    new Link(5, 200, 200, 'green', '#000000'),
+  ]);
 
   setInterval(function() {
     context.fillStyle = "gray";
     context.fillRect(0, 0, 500, 500);
 
-    //draggable_segment1.update();
-    draggable_segment2.update();
+    chain.update();
   }, 30);
 
   var mouseX = 0, mouseY = 0;
@@ -42,9 +39,9 @@ function draw_drag_toy() {
     context.stroke();
   }
 
-  function DraggableCircle(radius, x, y, fillColor, strokeColor) {
+  function Link(radius, x, y, fillColor, strokeColor) {
     var startX = 0, startY = 0;
-    var drag = false;
+    var dragging = false;
     this.x = x;
     this.y = y;
     this.neighbors = [];
@@ -55,31 +52,29 @@ function draw_drag_toy() {
         var right = this.x + radius;
         var top = this.y - radius;
         var bottom = this.y + radius;
-        if (!drag) {
+        if (!dragging) {
+          // offset from center of circle where the mouse was clicked
           startX = mouseX - this.x;
           startY = mouseY - this.y;
         }
         if (mouseX < right && mouseX > left && mouseY < bottom && mouseY > top){
-          drag = true;
+          dragging = true;
         }
       } else {
-        drag = false;
+        dragging = false;
       }
 
-      if (drag) {
+      if (dragging) {
         var oldx = this.x;
         var oldy = this.y;
         this.x = mouseX - startX;
         this.y = mouseY - startY;
 
-        //if (this.name == 'dc2')
         for (var i = 0; i < this.neighbors.length; i++) {
           var neighbor = this.neighbors[i];
           var adj = this.x - neighbor.x;
           var opp = this.y - neighbor.y;
           var hyp = Math.sqrt(Math.pow(oldx - neighbor.x, 2) + Math.pow(oldy - neighbor.y, 2));
-          //var hyp = 140;
-          //var theta = Math.atan(opp/adj);
           var theta = Math.atan2(opp, adj);
           var new_adj = Math.cos(theta) * hyp;
           var new_opp = Math.sin(theta) * hyp;
@@ -94,30 +89,36 @@ function draw_drag_toy() {
     this.update = update.bind(this);
   }
 
-  function DraggableSegment(dc1, dc2) {
-    this.dc1 = dc1;
-    this.dc2 = dc2;
+  function DraggableChain(circles) {
+    this.circles = circles;
 
-    dc1.name = 'dc1';
-    dc2.name = 'dc2';
-    dc1.neighbors.push(dc2);
-    dc2.neighbors.push(dc1);
+    for (var i = 0; i < circles.length; i++) {
+      if (i > 0) {
+        circles[i].neighbors.push(circles[i - 1]);
+        //circles[i].left_neighbor = circles[i - 1];
+      }
+      if (i < circles.length - 1) {
+        circles[i].neighbors.push(circles[i + 1]);
+        //circles[i].right_neighbor = circles[i + 1];
+      }
+    }
 
     function update() {
-      this.dc1.update();
-      this.dc2.update();
+      for (var i = 0; i < this.circles.length; i++) {
+        var cur = this.circles[i];
+        
+        cur.update();
 
-      context.beginPath();
-      context.moveTo(this.dc1.x, this.dc1.y);
-      context.lineTo(this.dc2.x, this.dc2.y);
-      context.stroke();
+        if (i > 0) {
+          var prev = this.circles[i - 1];
+          context.beginPath();
+          context.moveTo(prev.x, prev.y);
+          context.lineTo(cur.x, cur.y);
+          context.stroke();
+        }
+      }
     }
 
     this.update = update.bind(this);
-  }
-
-  function DraggableChain(segments) {
-    this.segments = segments;
-
   }
 }
