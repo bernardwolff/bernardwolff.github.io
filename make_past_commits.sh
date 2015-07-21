4b6d0d274c7b98cedc1116c5834cd3627d359ac2
Bernard Wolff <bernard.wolff@tntp.org>
2015-07-21 15:26:38 -0400

    past

---
+++ make_past_commits.sh
@@ -0,0 +1,16 @@
+YEAR_AGO=$(date --date="1 year ago" +"%Y%m%d")
+DATE="$YEAR_AGO"
+
+while [ "$DATE" != "$(date +'%Y%m%d')" ]
+do
+  DATE=$(date --date="$DATE + 1 day" +"%Y%m%d")
+  echo $DATE
+
+  for i in {1..30}
+  do
+    echo "$DATE $i" > past_commit
+    git commit -am "$DATE $i" --date="$(date --date=$DATE)"
+  done
+done
+
+git push
